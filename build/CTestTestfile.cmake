f31660d2c322a81a4139c1db87b46d85c88ebbb3
77zz1005 <1316946584@qq.com>
2023-10-23 19:44:25 +0800

    slam_localization

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/wrs/nav_tutorial_ws/src
+# Build directory: /home/wrs/nav_tutorial_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("slam_pkg")
+subdirs("where_dependencis")
+subdirs("demo_pkg")
+subdirs("gazebo_demo")
---
+++ build/where_dependencis/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wrs/nav_tutorial_ws/src/where_dependencis
+# Build directory: /home/wrs/nav_tutorial_ws/build/where_dependencis
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
